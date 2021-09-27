ed2cf191c4fbf08e5bf7693e5528319a1546db1e
masibw <masi19bw@gmail.com>
2021-09-27 16:12:57 +0900

    Show check contrast results. (#2)
    * Display results
    
    * Add README.md

--- usecase/checkContrast.go
+++ usecase/checkContrast.go
@@ -2,6 +2,8 @@ package usecase
 
 import (
 	"fmt"
+	"image"
+	"image/color"
 
 	"github.com/pkg/errors"
 
@@ -39,7 +41,7 @@ func CheckContrast(video *gocv.VideoCapture) error {
 			return errors.Wrapf(err, "image set error")
 		}
 
-		outs, err := client.GetBoundingBoxes(gosseract.RIL_WORD)
+		outs, err := client.GetBoundingBoxes(gosseract.RIL_SYMBOL)
 		if err != nil {
 			return errors.Wrapf(err, "failed to get bounding boxes")
 		}
@@ -52,7 +54,7 @@ func CheckContrast(video *gocv.VideoCapture) error {
 			fmt.Println("out: ", out)
 
 			// Take the color of the center of the text
-			//TODO: In case of something like ○ (but not text), you can't get the exact color, so you need to combine different methods.
+			//TODO: In case of something like n, you can't get the exact color, so you need to combine different methods.
 			textColor := imgImage.At((out.Box.Min.X+out.Box.Max.X)/2, (out.Box.Min.Y+out.Box.Max.Y)/2)
 			tr, tg, tb, ta := textColor.RGBA()
 
@@ -77,9 +79,21 @@ func CheckContrast(video *gocv.VideoCapture) error {
 			}
 			backgroundRGB := brgba.ToRGB()
 			ratio := contrast.GetRatio(textRGB, backgroundRGB)
-
 			fmt.Println("ratio: ", ratio)
-			fmt.Println("level: ", contrast.GetLevel(ratio))
+			level := contrast.GetLevel(ratio)
+			fmt.Println("level: ", level)
+			r := out.Box
+			blue := color.RGBA{B: 255}
+			gocv.Rectangle(&frame, r, blue, 3)
+			if level == "-" || level == "A" {
+				r := out.Box
+				red := color.RGBA{R: 255}
+				gocv.Rectangle(&frame, r, red, 3)
+				size := gocv.GetTextSize("Low Contrast", gocv.FontHersheyPlain, 1.2, 2)
+				pt := image.Pt((r.Min.X+r.Max.X)/2-(size.X/2), r.Min.Y-2)
+				gocv.PutText(&frame, "Low Contrast", pt, gocv.FontHersheyPlain, 1.2, red, 2)
+			}
+
 		}
 
 		//TODO: Display the results in some way
